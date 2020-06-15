9d5d01381fe4901d2885dbe49532d18f1193d782
Sarah Peters <sarahpieters15@gmail.com>
2020-06-15 16:12:10 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,10 +24,8 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-shipping_manifest["muskets" = 2, "gun powder" = 4]
-
-
-  # add 4 gun powder to the shipping_manifest hash below
+shipping_manifest["muskets"] = 2
+shipping_manifest["gun powder"] = 4
 
 
 
